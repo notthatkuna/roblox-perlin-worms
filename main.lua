61dd89ad7e00880b238f63a600ddf81791802057
jaelyn <74612839+notthatkuna@users.noreply.github.com>
2021-08-08 16:15:22 -0400

    Add more naturality and randomness to the algo
    Now allows the highest of highs in resolution by making up for the distance between the parts by tweening a larger part from the last part to the current part. This also clones the part every wait(resolution/10) and adds a light inside of the clone to light up the cave, but also not make it too bright. Resolution makeup also uses a random BALLSIZE variable when filling terrain, which I personally thought added more naturality. The last file algorithm just did not look right.

--- main.lua
+++ main.lua
@@ -17,14 +17,14 @@ local R = 0
 local switch = 1
 
 
-local Seed = 1 -- Changes cave generation a lot
+local Seed = math.random(1,1616) -- Changes cave generation a lot
 
 
 print("Seed is: "..Seed)
-local Resolution = 3 -- How far away each part will be from eachother, keep it low because higher numbers might break everything and it will look very messy
+local Resolution = math.random(5,100) -- How far away each part will be from eachother, keep it low because higher numbers might break everything and it will look very messy
 local NumWorm = 1
 local lastPart = nil
-while wait() do
+while wait(1) do
     local SIZEOF_BALL = math.random(10,20)
     local IS_WATER_WORM = false
 	NumWorm = NumWorm+1
@@ -36,7 +36,7 @@ while wait() do
 	local Dist = (math.noise(NumWorm/Resolution+WormCF.p.magnitude,Seed)+.5)*500
 
 	for i = 1,Dist do
-        wait(MASTERWAIT)
+		wait(.6)
         if R == 0 then
             switch = 1
         elseif R == 255 then
@@ -65,11 +65,52 @@ while wait() do
         -- Connect.Transparency = 1
         -- Part.Transparency = 1
         local light = Instance.new("PointLight",Part)
-        light.Brightness = .5
-        light.Range = 15
-        lastPart = Part
+        light.Brightness = 1
+		light.Range = 30
+		if lastPart then
+			local p2 = Instance.new("Part", workspace)
+			p2.Anchored = true
+			p2.Size = Part.Size + Vector3.new(10,10,10)
+			p2.Position = lastPart.Position
 
-        workspace.Terrain:FillBall(Part.Position,SIZEOF_BALL,Enum.Material.Air)
+			local ts = game:GetService("TweenService")
+			local tween = ts:Create(p2, TweenInfo.new(1), {CFrame = Part.CFrame})
+			tween:Play()
+			local comp = false
+			tween.Completed:Connect(function()
+				comp = true
+			end)
+			spawn(function()
+				while not comp do
+					wait()
+					SIZEOF_BALL = math.random(7,25)
+					workspace.Terrain:FillBall(p2.Position,SIZEOF_BALL,Enum.Material.Air)
+					
+				end
+			end)
+			spawn(function()
+				while not comp do
+					wait(Resolution/10)
+					local p2c = p2:Clone()
+					p2c.Parent = workspace
+					p2c.Transparency = .95
+					p2c.Size = p2c.Size - Vector3.new(5,5,5)
+					local light = Instance.new("PointLight",p2c)
+					light.Brightness = .3
+					light.Range = 30
+				end
+			end)
+		end
+        
+
+
+        if (Connect.Orientation.Z > -20 and Connect.Orientation.Z < 20) and Part.Position.Y < workspace.TOP.Position.Y and Part.Position.Y > workspace.BOTTOM.Position.Y and IS_WATER_WORM then
+            workspace.Terrain:FillBall(Part.Position,SIZEOF_BALL,Enum.Material.Water)
+        else
+            workspace.Terrain:FillBall(Part.Position,SIZEOF_BALL,Enum.Material.Air)
+		end
+		lastPart = Part
+		
 	end
     lastPart = nil
 end
